742b90d83d387c379b1b8c37316ef57496ca9f2f
albmarques <112758494+albmarques@users.noreply.github.com>
2023-04-18 09:42:00 -0300

    Update Fatura.java

--- atividade_calculo_imposto/atividade_1/src/atividade_1/Fatura.java
+++ atividade_calculo_imposto/atividade_1/src/atividade_1/Fatura.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package atividade_1;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author FATEC ZONA LESTE
- */
 public class Fatura {
     double consumo;
     double fornecimento;
